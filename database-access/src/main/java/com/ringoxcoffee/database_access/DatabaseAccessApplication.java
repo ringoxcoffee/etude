21018b934bd34ba8562ece92885a5f632e625fa6
ringoxcoffee <ringoxcoffee@gmail.com>
2024-12-14 22:38:25 +0900

    add a initialized module: database-access

---
+++ database-access/src/main/java/com/ringoxcoffee/database_access/DatabaseAccessApplication.java
@@ -0,0 +1,13 @@
+package com.ringoxcoffee.database_access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseAccessApplication.class, args);
+	}
+
+}
---
+++ database-access/src/test/java/com/ringoxcoffee/database_access/DatabaseAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ringoxcoffee.database_access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
