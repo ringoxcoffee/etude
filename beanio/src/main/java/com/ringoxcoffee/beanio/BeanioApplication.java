290b6fb4731149379a6f39bc6602d5cf32d8c42b
ringoxcoffee <ringoxcoffee@gmail.com>
2024-12-14 22:43:18 +0900

    add a initialized module: beanio

---
+++ beanio/src/main/java/com/ringoxcoffee/beanio/BeanioApplication.java
@@ -0,0 +1,13 @@
+package com.ringoxcoffee.beanio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanioApplication.class, args);
+	}
+
+}
